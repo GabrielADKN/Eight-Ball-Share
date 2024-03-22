fdc7fcc1660ed505e63055874eaaf20964ab8308
gabrieladkn <adoukonoubienheureux@gmail.com>
2024-03-22 00:30:00 -0400

    eightBall game

---
+++ eightBall/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import EightBall from './EightBall'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='App'>
+      <EightBall/>
+    </div>
+  )
+}
+
+export default App
